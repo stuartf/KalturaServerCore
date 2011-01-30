31b3b97109882bc1b2705b1970acf39164023add
Anatol Schwartz <anatol.schwartz@kaltura.com>
2011-01-30 09:16:50 +0000

    updated to cassiopeia plugin mode
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/cassiopeia_2011_01_27@56722 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/segmenter/lib/SegmenterConversionEngineType.php
+++ plugins/segmenter/lib/SegmenterConversionEngineType.php
@@ -3,14 +3,36 @@
  * @package api
  * @subpackage enum
  */
-class SegmenterConversionEngineType implements IKalturaPluginEnum, conversionEngineType
+class SegmenterConversionEngineType extends KalturaConversionEngineType
+
 {
 	const SEGMENTER = 'Segmenter';
+	
+	/**
+	 * @var SegmenterConversionEngineType
+	 */
+	protected static $instance;
+
+	/**
+	 * @return SegmenterConversionEngineType
+	 */
+	public static function get()
+	{
+		if(!self::$instance)
+			self::$instance = new SegmenterConversionEngineType();
+			
+		return self::$instance;
+	}
 	
 	public static function getAdditionalValues()
 	{
 		return array(
 			'SEGMENTER' => self::SEGMENTER
 		);
+	}
+	
+	public function getPluginName()
+	{
+		return SegmenterPlugin::getPluginName();
 	}
 }
