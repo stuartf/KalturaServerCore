158f5da14ee5f2979c80926b72d891825e730a35
svnuser <svnuser@kaltura.com>
2011-02-03 10:18:59 +0000

    added support for apple http streaming
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/cassiopeia_2011_01_27@57124 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/storage/urlMgr/kLocalPathUrlManager.php
+++ alpha/apps/kaltura/lib/storage/urlMgr/kLocalPathUrlManager.php
@@ -16,7 +16,45 @@ class kLocalPathUrlManager extends kUrlManager
 		
 		if($this->protocol == StorageProfile::PLAY_FORMAT_RTMP)
 			$url = preg_replace('/\.[\w]+$/', '', $url);
+
+		if ($this->protocol == StorageProfile::PLAY_FORMAT_APPLE_HTTP)
+			$url .= "/playlist.m3u8";
 		
 		return $url;
 	}
+
+	public function getFlavorAssetUrl(flavorAsset $flavorAsset)
+	{
+		$partnerId = $flavorAsset->getPartnerId();
+		$subpId = $flavorAsset->getentry()->getSubpId();
+		$partnerPath = myPartnerUtils::getUrlForPartner($partnerId, $subpId);
+		$flavorAssetId = $flavorAsset->getId();
+		
+		$this->setFileExtension($flavorAsset->getFileExt());
+		$this->setContainerFormat($flavorAsset->getContainerFormat());
+	
+		$url = "$partnerPath/serveFlavor/flavorId/$flavorAssetId";
+		
+		if($this->seekFromTime)
+			$url .= "/seekFrom/$this->seekFromTime";
+			
+		if($this->clipTo)
+			$url .= "/clipTo/$this->clipTo";
+		
+                if($this->protocol == StorageProfile::PLAY_FORMAT_RTMP)
+		{
+			$syncKey = $flavorAsset->getSyncKey(flavorAsset::FILE_SYNC_FLAVOR_ASSET_SUB_TYPE_ASSET);
+			$fileSync = kFileSyncUtils::getReadyInternalFileSyncForKey($syncKey);
+			$url = $this->getFileSyncUrl($fileSync);
+			if ($this->extention && strtolower($this->extention) != 'flv' ||
+                        	$this->containerFormat && strtolower($this->containerFormat) != 'flash video')	
+			{
+				$url = "mp4:$url";
+			}
+		}
+		
+		$url = str_replace('\\', '/', $url);
+		return $url;
+	}
+
 }
