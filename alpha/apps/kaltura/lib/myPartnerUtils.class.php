29bb625154bd3e8d9892cc9748444c38c8cc704e
svnuser <svnuser@kaltura.com>
2011-02-13 06:56:43 +0000

    add isset to HTTPS header check to prevent notice in log
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/cassiopeia_2011_01_27@57616 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/myPartnerUtils.class.php
+++ alpha/apps/kaltura/lib/myPartnerUtils.class.php
@@ -366,7 +366,7 @@ class myPartnerUtils
 	public static function getCdnHost ( $partner_id, $protocol = null )
 	{
 		// in case the request came through https, force https url
-		if (@$_SERVER['HTTPS'] == 'on')
+		if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on')
 			$protocol = 'https';
 
 		$partner = PartnerPeer::retrieveByPK( $partner_id );
