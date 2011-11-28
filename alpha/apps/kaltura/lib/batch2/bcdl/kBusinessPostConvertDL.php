ea603ee3046e9e1fbaa8587a7d9781f12460ff77
Anatol Schwartz <anatol.schwartz@kaltura.com>
2011-11-28 15:42:24 +0000

    fwr: ABCN merge - remote convert
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/eagle_2011_11_22@73390 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/batch2/bcdl/kBusinessPostConvertDL.php
+++ alpha/apps/kaltura/lib/batch2/bcdl/kBusinessPostConvertDL.php
@@ -115,7 +115,8 @@ class kBusinessPostConvertDL
 		{
 			KalturaLog::err($e->getMessage());
 		}
-				
+		KalturaLog::debug("profile [" . $profile->getId() . "]");
+								
 		$currentReadyBehavior = self::getReadyBehavior($currentFlavorAsset, $profile);
 		KalturaLog::debug("Current ready behavior [$currentReadyBehavior]");
 		
@@ -124,7 +125,14 @@ class kBusinessPostConvertDL
 			$rootBatchJob = $dbBatchJob->getRootJob();
 		if($rootBatchJob)
 			KalturaLog::debug("root batch job id [" . $rootBatchJob->getId() . "] type [" . $rootBatchJob->getJobType() . "]");
-		
+		// update the root job end exit
+		if($rootBatchJob && $rootBatchJob->getJobType() == BatchJobType::REMOTE_CONVERT)
+		{
+			KalturaLog::debug("finish remote convert root job");
+			kJobsManager::updateBatchJob($rootBatchJob, BatchJob::BATCHJOB_STATUS_FINISHED);
+			return $dbBatchJob;
+		}
+				
 		// update the root job end exit
 		if($rootBatchJob && $rootBatchJob->getJobType() == BatchJobType::BULKDOWNLOAD)
 		{
