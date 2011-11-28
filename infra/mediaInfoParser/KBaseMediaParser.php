3daa06329c6354e9b8277a6b0eb85ba1ce684be5
Anatol Schwartz <anatol.schwartz@kaltura.com>
2011-11-28 16:16:04 +0000

    fwr: ABCN merge from trunk - remote media info support
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/eagle_2011_11_22@73410 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- infra/mediaInfoParser/KBaseMediaParser.php
+++ infra/mediaInfoParser/KBaseMediaParser.php
@@ -5,10 +5,34 @@
  */
 abstract class KBaseMediaParser
 {
+	const MEDIA_PARSER_TYPE_MEDIAINFO = '0';
+	const MEDIA_PARSER_TYPE_FFMPEG = '1';
+	
 	/**
 	 * @var string
 	 */
 	protected $filePath;
+	
+	/**
+	 * @param string $type
+	 * @param string $filePath
+	 * @param KSchedularTaskConfig $taskConfig
+	 * @return KBaseMediaParser
+	 */
+	public static function getParser($type, $filePath, KSchedularTaskConfig $taskConfig, KalturaBatchJob $job)
+	{
+		switch($type)
+		{
+			case self::MEDIA_PARSER_TYPE_MEDIAINFO:
+				return new KMediaInfoMediaParser($filePath, $taskConfig->params->mediaInfoCmd);
+				
+			case self::MEDIA_PARSER_TYPE_FFMPEG:
+				return new KFFMpegMediaParser($filePath, $taskConfig->params->FFMpegCmd);
+				
+			default:
+				return KalturaPluginManager::loadObject('KBaseMediaParser', $type, array($job, $taskConfig));
+		}
+	}
 	
 	/**
 	 * @param string $filePath
@@ -26,13 +50,22 @@ abstract class KBaseMediaParser
 	 */
 	public function getMediaInfo()
 	{
+		$output = $this->getRawMediaInfo();
+		return $this->parseOutput($output);
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getRawMediaInfo()
+	{
 		$cmd = $this->getCommand();
 		KalturaLog::debug("Executing '$cmd'");
 		$output = shell_exec($cmd);
 		if (trim($output) === "")
 			throw new Exception("Failed to parse media using " . get_class($this));
 			
-		return $this->parseOutput($output);
+		return $output;
 	}
 	
 	/**
