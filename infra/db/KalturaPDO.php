bf1c1a930c5054a3af86ab7bceb4d69f69b3a0d0
svnuser <svnuser@kaltura.com>
2011-12-14 08:38:08 +0000

    qnd: add comment to sql query
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/eagle_2011_11_22@74318 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- infra/db/KalturaPDO.php
+++ infra/db/KalturaPDO.php
@@ -1,38 +1,115 @@
 <?php
+
 /**
  *  @package infra
  *  @subpackage DB
  */
 class KalturaPDO extends PropelPDO
 {
+	/**
+	 * Attribute to use to set logged info for Kaltura logger
+	 */
+	const KALTURA_ATTR_NAME = -1001;
+	
+	protected static $comment = null;
+	
+	protected $connectionName = null;
+	
+	protected $enableComments = true;
+	
+	/* (non-PHPdoc)
+	 * @see PDO::__construct()
+	 */
 	public function __construct($dsn, $username = null, $password = null, $driver_options = array())
-	{
+	{	
+		if(isset($driver_options[self::KALTURA_ATTR_NAME]))
+			$this->connectionName = $driver_options[self::KALTURA_ATTR_NAME];
+			
 		$connStart = microtime(true);
+
 		parent::__construct($dsn, $username, $password, $driver_options);
+
 		KalturaLog::debug("conn took - ". (microtime(true) - $connStart). " seconds to $dsn");
+
 		$this->setAttribute(PDO::ATTR_STATEMENT_CLASS, array('KalturaStatement'));
 	}
+
+	public function setCommentsEnabled($enabled) 
+	{
+		$this->enableComments = $enabled;
+	}
+
+	protected function getComment() 
+	{
+		if(!$this->enableComments)
+			return '';
+			
+		if(!self::$comment)
+		{
+			$uniqueId = new UniqueId();
+			self::$comment = (isset($_SERVER["HOSTNAME"]) ? $_SERVER["HOSTNAME"] : '');
+			self::$comment .= "[$uniqueId]";
+		}
+		
+		return '/* ' . self::$comment . "[$this->connectionName] */ ";
+	}
 	
+	/* (non-PHPdoc)
+	 * @see PropelPDO::prepare()
+	 */
+	public function prepare($sql, $driver_options = array())
+	{
+		$comment = $this->getComment();
+		$sql = $comment . $sql;
+		
+		return parent::prepare($sql, $driver_options);
+	}
+	
+	/* (non-PHPdoc)
+	 * @see PDO::exec()
+	 */
 	public function exec($sql)
 	{
 		KalturaLog::debug($sql);
-		return parent::exec($sql);
+		
+		$comment = $this->getComment();
+		$sql = $comment . $sql;
+		
+		try
+		{
+			return parent::exec($sql);
+		}
+		catch(PropelException $pex)
+		{
+			KalturaLog::alert($pex->getMessage());
+			throw new PropelException("Database error");
+		}
 	}
 
-	/**
+	/* (non-PHPdoc)
 	 * @see PDO::query()
-	 * @return KalturaStatement
 	 */
 	public function query()
 	{
-		$args	= func_get_args();
+		$args = func_get_args();
 		
 		$sql = $args[0];
 		KalturaLog::debug($sql);
 		
-		if (version_compare(PHP_VERSION, '5.3', '<'))
-			return call_user_func_array(array($this, 'parent::query'), $args);
+		$comment = $this->getComment();
+		$sql = $comment . $sql;
 		
-		return call_user_func_array('parent::query', $args);
+		try
+		{
+			if (version_compare(PHP_VERSION, '5.3', '<'))
+				return call_user_func_array(array($this, 'parent::query'), $args);
+			
+			return call_user_func_array('parent::query', $args);
+		}
+		catch(PropelException $pex)
+		{
+			KalturaLog::alert($pex->getMessage());
+			throw new PropelException("Database error");
+		}
 	}
 }
