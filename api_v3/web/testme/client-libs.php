ae0d095fd38da775cd307d1799355b3d8378af8c
Eran Kornblau <eran.kornblau@kaltura.com>
2012-01-01 15:33:10 +0000

    qnd: client libraries page - download does not work in IE
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/eagle_2011_11_22@74874 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/web/testme/client-libs.php
+++ api_v3/web/testme/client-libs.php
@@ -91,8 +91,7 @@
 							{
 								?>
 									<div class="download-button <?php echo $clientName; ?>-btn">
-										<a href="http://<?php echo kConf::get('cdn_host'); ?>/content/clientlibs/<?php echo $clientName.'_'.$schemaGenDate; ?>.tar.gz" target="_blank" >
-											<button class="download-btn" title="Single class <?php echo $clientName; ?> client library"></button>
+										<a href="http://<?php echo kConf::get('cdn_host'); ?>/content/clientlibs/<?php echo $clientName.'_'.$schemaGenDate; ?>.tar.gz" target="_blank" title="Single class <?php echo $clientName; ?> client library" class="download-btn">
 										</a>
 									</div>
 								<?php 
