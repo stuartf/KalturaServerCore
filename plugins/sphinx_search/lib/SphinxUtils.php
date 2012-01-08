dbbbd22dac9bd32300394ebbb1bd1ae437a42988
Eran Kornblau <eran.kornblau@kaltura.com>
2012-01-08 11:04:03 +0000

    qnd: added support for searching custom data with " (exact match) and ! (and not)
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/eagle_2011_11_22@75091 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/sphinx_search/lib/SphinxUtils.php
+++ plugins/sphinx_search/lib/SphinxUtils.php
@@ -5,20 +5,16 @@ class SphinxUtils
 
 	public static function escapeString($str, $iterations = 2)
 	{
-		// NOTE: it appears that sphinx performs double decoding on SELECT values, so we perform 2 iterations.
-		//		iterations should be set to 1 by default, if they fix it in some future release.
-		$from = array ('\\',	'(',	')',	'|',	'-',	'@',	'~',	'&',	'/',	'^',	'$',	'=',	'_',	'%', 	'\'',	);
-		$to   = array ('\\\\',	'\(',	'\)',	'\|',	'\-',	'\@',	'\~',	'\&',	'\/',	'\^',	'\$',	'\=',	'\_',	'\%',	'\\\'',	);
-		for ($iter = 0; $iter < $iterations; $iter++)
-		{
-			$str = str_replace($from, $to, $str);
-		}
-		
-		// The following characters are escaped only once since we want to enable clients to use them
-		//	" - search for exact match, ! - AND NOT
-		$from = array ('"',		'!',	);
-		$to   = array ('\\"',	'\\!',	);
-		$str = str_replace($from, $to, $str);
-		return $str;
+		// NOTE: it appears that sphinx performs double decoding on SELECT values, so we encode twice.
+		//		" and ! are escaped once to enable clients to use them, " = exact match, ! = AND NOT
+		//	This code could have been implemented more elegantly using array_map, but this implementation is the fastest
+		$from 		= array ('\\', 		'"', 		'!',		'(',		')',		'|',		'-',		'@',		'~',		'&',		'/',		'^',		'$',		'=',		'_',		'%', 		'\'',		);
+		$toSingle   = array ('\\\\', 	'\\"', 		'\\!',		'\\(',		'\\)',		'\\|',		'\\-',		'\\@',		'\\~',		'\\&',		'\\/',		'\\^',		'\\$',		'\\=',		'\\_',		'\\%',		'\\\'',		);
+		$toDouble   = array ('\\\\', 	'\\"', 		'\\!',		'\\\\\\(',	'\\\\\\)',	'\\\\\\|',	'\\\\\\-',	'\\\\\\@',	'\\\\\\~',	'\\\\\\&',	'\\\\\\/',	'\\\\\\^',	'\\\\\\$',	'\\\\\\=',	'\\\\\\_',	'\\\\\\%',	'\\\\\\\'',	);
+
+		if ($iterations == 2)
+			return str_replace($from, $toDouble, $str);
+		else
+			return str_replace($from, $toSingle, $str);
 	}
 }
