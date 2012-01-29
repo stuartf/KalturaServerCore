c2d726329ea058a01d705abcd8749b0a1eca0eaf
Hila Karimov <Hila.Karimov@kaltura.com>
2012-01-29 09:35:27 +0000

    fwr: mantis ticket 0014221- script for identifying country by IP address
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/eagle_2011_11_22@75622 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- scripts/testIP.php
+++ scripts/testIP.php
@@ -12,5 +12,5 @@ $myLocator = new myIPGeocoder();
 
 $countryAndRegion = $myLocator->iptocountryAndCode( $argv[1] );
 
-print_r("contry and region: " .$countryAndRegion, true);
+print_r("country and region: " .$countryAndRegion, true);
 
