1bbf8620065c3846aac6faf445473a8b7165c0c6
Assaf Berkovitz <Assaf.Berkovitz@kaltura.com>
2012-02-02 08:14:36 +0000

    qnd: mantis 14251. 1.Copying tags + description + media-info from temporery entry's assets to the original entry's assets                                   2.In asset.php: setting the new created asset's version to 1.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/eagle_2011_11_22@75791 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/batch2/bcdl/kBusinessConvertDL.php
+++ alpha/apps/kaltura/lib/batch2/bcdl/kBusinessConvertDL.php
@@ -61,6 +61,8 @@ public static function replaceEntry(entry $entry, entry $tempEntry = null)
 			if(isset($newAssets[$oldAsset->getType()]) && isset($newAssets[$oldAsset->getType()][$oldAsset->getFlavorParamsId()]))
 			{
 				$newAsset = $newAssets[$oldAsset->getType()][$oldAsset->getFlavorParamsId()];
+				//in situations were the flavor was created but failed to be converted it param id wont be added to the entry's flavor_params_ids field. so now when replacing the
+				//old entry's flavors with the temporery entry's flavors we should add their params ids to the entry's flavor_params_ids field.
 				$entry->addFlavorParamsId($oldAsset->getFlavorParamsId());
 				
 				/* @var $newAsset asset */
@@ -77,7 +79,8 @@ public static function replaceEntry(entry $entry, entry $tempEntry = null)
 				$oldAsset->setContainerFormat($newAsset->getContainerFormat());
 				$oldAsset->setSize($newAsset->getSize());
 				$oldAsset->setFileExt($newAsset->getFileExt());
-					
+				$oldAsset->setTags($newAsset->getTags());
+				$oldAsset->setDescription($newAsset->getDescription());
 				$oldAsset->incrementVersion();
 				$oldAsset->setStatus(asset::FLAVOR_ASSET_STATUS_READY);
 				$oldAsset->save();
@@ -87,6 +90,14 @@ public static function replaceEntry(entry $entry, entry $tempEntry = null)
 				
 				kFileSyncUtils::createSyncFileLinkForKey($oldFileSync, $newFileSync);
 				
+				$newFlavorMediaInfo = mediaInfoPeer::retrieveByFlavorAssetId($newAsset->getId());
+				if($newFlavorMediaInfo)
+				{
+					$oldFlavorNewMediaInfo = $newFlavorMediaInfo->copy();
+					$oldFlavorNewMediaInfo->setFlavorAssetId($oldAsset->getId());
+					$oldFlavorNewMediaInfo->setFlavorAssetVersion($oldAsset->getVersion());
+					$oldFlavorNewMediaInfo->save();
+				}
 				unset($newAssets[$oldAsset->getType()][$oldAsset->getFlavorParamsId()]);
 			}	
 			//If the old asset is not set for replacement by its paramsId and type, delete it.
