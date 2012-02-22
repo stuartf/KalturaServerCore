04bbde4ebeb249e2c28cf5da994c4627c13f3d72
Hila Karimov <Hila.Karimov@kaltura.com>
2012-02-22 12:25:16 +0000

    fwr: fix for mantis ticket 14343 - add RTMP prefix string to storage profiles
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/eagle_2011_11_22@76386 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/storage/kUrlManager.php
+++ alpha/apps/kaltura/lib/storage/kUrlManager.php
@@ -179,18 +179,18 @@ class kUrlManager
 	
 		if($this->protocol == StorageProfile::PLAY_FORMAT_RTMP)
 		{
+			$storageProfile = StorageProfilePeer::retrieveByPK($this->storageProfileId);
+			if ($storageProfile->getRTMPPrefix())
+			{
+			    $url = $storageProfile->getRTMPPrefix()."/". $url;
+			}
+			
 			if (($this->extention && strtolower($this->extention) != 'flv' ||
 				$this->containerFormat && strtolower($this->containerFormat) != 'flash video'))
 				{
 				    $url = "mp4:$url";
 				}
             
-			$storageProfile = StorageProfilePeer::retrieveByPK($this->storageProfileId);
-			if ($storageProfile->getRTMPPrefix())
-			{
-			    $url = $storageProfile->getRTMPPrefix()."/". $url;
-			}
-				
 			// when serving files directly via RTMP fms doesnt expect to get the file extension				
 			$url = str_replace('.mp4', '', str_replace('.flv','',$url));
 		}
