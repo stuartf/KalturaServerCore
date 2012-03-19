ad74b614ac3e541ce966812663b44c7a5e34be23
Hila Karimov <Hila.Karimov@kaltura.com>
2012-03-19 08:34:49 +0000

    fwr: return email ingestion job to usable state
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/eagle_2011_11_22@77247 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- batch/batches/EmailIngestion/KAsyncEmailIngestion.class.php
+++ batch/batches/EmailIngestion/KAsyncEmailIngestion.class.php
@@ -203,7 +203,7 @@ class KAsyncEmailIngestion extends KBatchBase {
 				
 				
 				// validate partner and get email profile
-				$email_profiles = $this->validePartnerAndGetProfile($curMail->header->toadd, $user.'@'.$host);
+				$email_profiles = $this->validePartnerAndGetProfile(array($curMail->header->fromadd), $user.'@'.$host);
 				if (!$email_profiles) {
 					// error validating partner
 					KalturaLog::err('Partner validation failed for ['.$curMail->header->msgid."] on [$user@$host] from [".$curMail->header->fromadd.'] with subject ['.$curMail->header->subject.']');
@@ -341,8 +341,9 @@ class KAsyncEmailIngestion extends KBatchBase {
 		// loop through all attachments
 		// ----------------------------
 		$num = 1;
+		$errorMsg = "";
 		foreach ($mailData->attachments as $cur_attach) {
-			
+			 
 			if ($profile->maxAttachmentsPerMail && ($num > $profile->maxAttachmentsPerMail)) {
 				KalturaLog::info ('Mail msg ['.$mailData->header->msgid.'] has more than ['.$profile->maxAttachmentsPerMail.'] attachments - ignoring the rest.');
 				$problems_happened = true;
@@ -404,13 +405,15 @@ class KAsyncEmailIngestion extends KBatchBase {
 			// upload file to the kaltura server
 			// ---------------------------------
 			try {
-				$tokenId = $this->getClient()->upload->upload(realpath($filename)); 
+				$requestResults = $this->createUploadTokenAndUpload($profile, $filename);
+				
+				list ($token, $upload) = $requestResults;
 			}
 			catch (Exception $e) {
-				$tokenId = null;
+				$token = null;
 				KalturaLog::err($e->getMessage());
 			}
-			if ($tokenId == null || !$tokenId) {
+			if ($token->id == null || !$token->id) {
 				KalturaLog::err("Error uploading [$filename] to the kaltura server.");
 				$problems_happened = true;
 				$failures->upload_failed = true;
@@ -424,14 +427,14 @@ class KAsyncEmailIngestion extends KBatchBase {
 			// create a new entry from the uploaded file
 			// -----------------------------------------
 			try {
-				$newEntry = $this->getClient()->EmailIngestionProfile->addMediaEntry($mediaEntry, $tokenId, $profile->id, $mailData->header->fromadd, $mailData->header->msgid);
+				$newEntry = $this->getClient()->EmailIngestionProfile->addMediaEntry($mediaEntry, $token->id, $profile->id, $mailData->header->fromadd, $mailData->header->msgid);
 			}
 			catch (Exception $e) {
 				$newEntry = null;
 				KalturaLog::err($e->getMessage());
 			}
 			if ($newEntry == null || !$newEntry) {
-				KalturaLog::err("Error adding entry from uploaded file [$filename], token [$tokenId].");
+				KalturaLog::err("Error adding entry from uploaded file [$filename], token [".$token->id."]");
 				$problems_happened = true;
 				$failures->add_entry_failed = true;
 				// delete the temporary file from the disk
@@ -455,7 +458,30 @@ class KAsyncEmailIngestion extends KBatchBase {
 		return !$problems_happened;
 	}
 
-
+	/**
+     * 
+     * Creates an upload token and uploads the file using the token.
+     * @param KalturaEmailIngestionProfile $profile
+     * @param string $filename
+     */
+    private function createUploadTokenAndUpload ($profile, $filename)
+    {
+        $this->impersonate($profile->partnerId);
+			    
+	    $this->getClient()->startMultiRequest();
+	    
+	    $uploadToken = new KalturaUploadToken(); 
+	    
+	    $uploadToken->fileName = $filename;
+	    
+	    $uploadToken->fileSize = filesize($filename);
+	    
+	    $uploadToken = $this->getClient()->uploadToken->add($uploadToken);
+	    
+		$this->getClient()->uploadToken->upload($uploadToken->id, realpath($filename));
+		
+		return $this->getClient()->doMultiRequest();
+    }
 
 
 
