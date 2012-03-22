981cb63634a433623c1bb575783cf2dab5beb2c9
Jonathan Kanarek <Jonathan.Kanarek@kaltura.com>
2012-03-22 19:23:05 +0000

    def: 1983 enable one restriction to be affected by other restrictions, required for preview restriction.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/eagle_2011_11_22@77437 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/lib/types/accessControl/KalturaBaseRestriction.php
+++ api_v3/lib/types/accessControl/KalturaBaseRestriction.php
@@ -3,14 +3,16 @@
  * @package api
  * @subpackage objects
  * @deprecated use KalturaRule instead
+ * @abstract
  */
 class KalturaBaseRestriction extends KalturaObject
 {
 	/**
+	 * @param KalturaRestrictionArray $restrictions enable one restriction to be affected by other restrictions
 	 * @return kAccessControlRestriction
 	 * @abstract must be implemented
 	 */
-	public function toRule()
+	public function toRule(KalturaRestrictionArray $restrictions)
 	{
 		throw new KalturaAPIException(KalturaErrors::OBJECT_TYPE_ABSTRACT, get_class($this));
 	}
