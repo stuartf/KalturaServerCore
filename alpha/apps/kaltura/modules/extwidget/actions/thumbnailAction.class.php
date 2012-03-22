e076759403a2d3c2fa38c33395027dd1f2133767
Omri Bendor <Omri.Bendor@kaltura.com>
2012-03-22 10:48:41 +0000

    qnd: reverted my last commit.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/eagle_2011_11_22@77422 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/modules/extwidget/actions/thumbnailAction.class.php
+++ alpha/apps/kaltura/modules/extwidget/actions/thumbnailAction.class.php
@@ -266,19 +266,15 @@ class thumbnailAction extends sfAction
 						$origFlavorAsset = assetPeer::retrieveOriginalByEntryId($entry_id);
 						if ($origFlavorAsset) {
 							$syncKey = $origFlavorAsset->getSyncKey ( flavorAsset::FILE_SYNC_FLAVOR_ASSET_SUB_TYPE_ASSET );
+							$remoteFileSync = kFileSyncUtils::getOriginFileSyncForKey ( $syncKey, false );
 							
-							list($readyFileSync,$isLocal) = kFileSyncUtils::getReadyFileSyncForKey( $syncKey, TRUE, FALSE );
-							if ($readyFileSync) {
-								if ($isLocal) {
-									KalturaLog::log ( "ERROR - Trying to redirect to myself - stop here." );
-									KExternalErrors::dieError ( KExternalErrors::MISSING_THUMBNAIL_FILESYNC );
-								}
-								//Ready fileSync is on the other DC - dumping
-								kFile::dumpApiRequest ( kDataCenterMgr::getRemoteDcExternalUrlByDcId ( 1 - kDataCenterMgr::getCurrentDcId () ) );
+							if ($remoteFileSync && $remoteFileSync->getDc () == kDataCenterMgr::getCurrentDcId ()) {
+								KalturaLog::log ( "ERROR - Trying to redirect to myself - stop here." );
+								KExternalErrors::dieError ( KExternalErrors::MISSING_THUMBNAIL_FILESYNC );
 							}
-							throw new kFileSyncException('No ready fileSync found on any DC',kFileSyncException::FILE_NOT_FOUND);
 						}
-						// problem could be due to replication lag
+						// problem could be due to replocation lag
+						kFile::dumpApiRequest ( kDataCenterMgr::getRemoteDcExternalUrlByDcId ( 1 - kDataCenterMgr::getCurrentDcId () ) );
 					}
 				}
 			} 
