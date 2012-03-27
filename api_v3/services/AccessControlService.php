3795bc6ee4f8c95eb2a57a9d451adfc6b78ec119
Jonathan Kanarek <Jonathan.Kanarek@kaltura.com>
2012-03-27 23:11:22 +0000

    def: 2000 prevent updating new access control profile using the old service
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/eagle_2011_11_22@77605 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- api_v3/services/AccessControlService.php
+++ api_v3/services/AccessControlService.php
@@ -60,12 +60,22 @@ class AccessControlService extends KalturaBaseService
 	 * @param int $id
 	 * @param KalturaAccessControl $accessControl
 	 * @return KalturaAccessControl
+	 * 
+	 * @throws KalturaErrors::ACCESS_CONTROL_ID_NOT_FOUND
+	 * @throws KalturaErrors::ACCESS_CONTROL_NEW_VERSION_UPDATE
 	 */
 	function updateAction($id, KalturaAccessControl $accessControl)
 	{
 		$dbAccessControl = accessControlPeer::retrieveByPK($id);
 		if (!$dbAccessControl)
 			throw new KalturaAPIException(KalturaErrors::ACCESS_CONTROL_ID_NOT_FOUND, $id);
+	
+		$rules = $dbAccessControl->getRulesArray();
+		foreach($rules as $rule)
+		{
+			if(!($rule instanceof kAccessControlRestriction))
+				throw new KalturaAPIException(KalturaErrors::ACCESS_CONTROL_NEW_VERSION_UPDATE, $id);
+		}
 		
 		if ($accessControl->name !== null)
 			$accessControl->validatePropertyMinLength("name", 1);
