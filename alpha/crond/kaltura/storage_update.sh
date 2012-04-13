588f8e49b89b9727fdbf4577b08b917bb56b3638
svnuser <svnuser@kaltura.com>
2012-04-13 13:30:50 +0000

    qnd: commented cfindEntriesSizes.php job
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/eagle_2011_11_22@78228 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/crond/kaltura/storage_update.sh
+++ alpha/crond/kaltura/storage_update.sh
@@ -2,5 +2,5 @@
 
 WHEN=$(date -d "yesterday" +%Y-%m-%d)
 
-php @APP_DIR@/scripts/findEntriesSizes.php $WHEN >> @LOG_DIR@/`hostname`-findEntriesSizes.log
-php @APP_DIR@/alpha/batch/batchPartnerUsage.php >> @LOG_DIR@/`hostname`-BatchPartnerUsage_upgradeProcess.log 2>&1
+#php /opt/kaltura/app/scripts/findEntriesSizes.php $WHEN >> /var/log/`hostname`-findEntriesSizes.log
+php /opt/kaltura/app/alpha/batch/batchPartnerUsage.php >> /var/log/`hostname`-BatchPartnerUsage_upgradeProcess.log 2>&1
