660d2b03eb0e10bcbb7857fa5d22e3ff212de35a
Hila Karimov <Hila.Karimov@kaltura.com>
2012-04-17 10:49:34 +0000

    fwr: reverting deployment scripts required for like feature
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/eagle_2011_11_22@78571 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/updates/sql/2012-04-17_kvote_add_status_column.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE kvote
-ADD `status` INTEGER,
-ADD `partner_id` INTEGER,
-ADD `type` INTEGER default 1,
-ADD `custom_data` TEXT,
-ADD KEY `entry_user_status_index`(`entry_id`, `kuser_id`, `status`)
-;
