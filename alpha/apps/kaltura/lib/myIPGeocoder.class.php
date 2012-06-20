cdcbad47a43b7a6b1305b0ceaf4fe2e36343678e
Eran Kornblau <eran.kornblau@kaltura.com>
2012-06-20 11:29:45 +0000

    qnd: added missing requires
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/eagle_2011_11_22@82818 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/lib/myIPGeocoder.class.php
+++ alpha/apps/kaltura/lib/myIPGeocoder.class.php
@@ -1,4 +1,5 @@
 <?php
+require_once(realpath(dirname(__FILE__)).'/../../../config/sfrootdir.php');
 require_once(dirname(__FILE__) . '/access_control/kGeoCoder.php');
 
 @include("IP2Location.inc.php");
