f2b7af4dce460e250a24d8f6ce98d4955f7c1c31
Ran Yefet <ran.yefet@kaltura.com>
2012-08-27 09:21:19 +0000

    qnd: fixed stripping protocol to support http & https
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/falcon_2012_08_20@86920 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/apps/kaltura/modules/kmc/actions/kmc4Action.class.php
+++ alpha/apps/kaltura/modules/kmc/actions/kmc4Action.class.php
@@ -83,9 +83,9 @@ class kmc4Action extends kalturaAction
 	
 	/** set values for template **/
 	$this->service_url = myPartnerUtils::getHost($this->partner_id);
-	$this->host = str_replace ( "http://" , "" , $this->service_url );
+	$this->host = $this->stripProtocol( $this->service_url );
 	$this->cdn_url = myPartnerUtils::getCdnHost($this->partner_id);
-	$this->cdn_host = str_replace ( "http://" , "" , $this->cdn_url );
+	$this->cdn_host = $this->stripProtocol( $this->cdn_url );
 	$this->rtmp_host = myPartnerUtils::getRtmpUrl($this->partner_id);
 	$this->flash_dir = $this->cdn_url . myContentStorage::getFSFlashRootPath ();
 	
@@ -163,6 +163,16 @@ class kmc4Action extends kalturaAction
 		
 		/** get templateXmlUrl for whitelabeled partners **/
 		//$this->appstudio_templatesXmlUrl = $this->getAppStudioTemplatePath();
+	}
+
+	private function stripProtocol( $url )
+	{
+		$url_data = parse_url( $url );
+		if( $url_data !== false ){
+			return $url_data['host'];
+		} else {
+			return $url;
+		}
 	}
 
 	private function getAppStudioTemplatePath()
