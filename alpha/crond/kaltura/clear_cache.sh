a9a71969d74ab1fdedfd5dafec96f5e523f4186e
Hila Karimov <Hila.Karimov@kaltura.com>
2012-09-30 10:04:18 +0000

    fwr: log file should be under Kaltura hierarchy and not under /var/log.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/falcon_2012_08_20@88185 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/crond/kaltura/clear_cache.sh
+++ alpha/crond/kaltura/clear_cache.sh
@@ -3,6 +3,7 @@
 # Store PID of script:
 # Match script without arguments
 LCK_FILE=/tmp/`basename $0`.lck
+LOG_FILE=/opt/kaltura/log/clear_cache.log
 
 if [ -f "${LCK_FILE}" ]; then
 
@@ -22,13 +23,13 @@ echo $$ > $LCK_FILE
 
 
 
-echo "`date +%s` start clean v3 `date`" >> /var/log/clear_cache.log
+echo "`date +%s` start clean v3 `date`" >> $LOG_FILE 2>&1
 #nice -n 19 find /tmp/cache_v3-600 -type f -mmin +1440 -name "cache*" -delete
 /usr/bin/ionice -c3 find /tmp/cache_v3-600 -type f -mmin +1440 -name "cache*" -delete
-echo "`date +%s` end clean v3 `date`" >> /var/log/clear_cache.log
+echo "`date +%s` end clean v3 `date`" >> $LOG_FILE 2>&1
 #nice -n 19 find /tmp/cache_v2 -type f -mmin +1440 -name "cache*" -delete
 /usr/bin/ionice -c3 find /tmp/cache_v2 -type f -mmin +1440 -name "cache*" -delete
-echo "`date +%s` end clean v2 `date`" >> /var/log/clear_cache.log
+echo "`date +%s` end clean v2 `date`" >> $LOG_FILE 2>&1
 #nice -n 19 find /tmp -maxdepth 0 -type f -mmin +1440 -name "php*" -delete
 /usr/bin/ionice -c3 find /tmp -maxdepth 1 -type f -mmin +1440 -name "php*" -delete
-echo "`date +%s` end clean php `date`" >> /var/log/clear_cache.log
+echo "`date +%s` end clean php `date`" >> $LOG_FILE 2>&1
