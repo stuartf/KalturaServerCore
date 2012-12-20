5b5608ec93c19f9646acaca226ae14897038733a
Hila Karimov <Hila.Karimov@kaltura.com>
2012-12-20 16:46:03 +0000

    fwr: port code from Gemini
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/falcon_2012_08_20@91898 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ plugins/event_notification/providers/email/lib/batch/recipient/KEmailNotificationCategoryRecipientEngine.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Engine which retrieves the email notification recipients for a category-related event.
+ * 
+ * @package plugins.emailNotification
+ * @subpackage Scheduler
+ */
+class KEmailNotificationCategoryRecipientEngine extends KEmailNotificationRecipientEngine
+{
+	/* (non-PHPdoc)
+	 * @see KEmailNotificationRecipientEngine::getRecipients()
+	 */ 
+	function getRecipients(array $contentParameters)
+	{
+		//List categoryKusers
+		$categoryUserList = $this->client->categoryUser->listAction($this->recipientJobData->categoryUserFilter, new KalturaFilterPager());
+		
+		$categoryUserIds = array();
+		foreach ($categoryUserList->objects as $categoryUser)
+			$categoryUserIds[] = $categoryUser->userId;
+		$userFilter = new KalturaUserFilter();
+		$userFilter->idIn = implode(',', $categoryUserIds);
+		$userList = $this->client->user->listAction($userFilter, new KalturaFilterPager());
+		
+		$recipients = array();
+		foreach ($userList->objects as $user)
+		{
+			/* @var $user KalturaUser */
+			$recipients[$user->email] = $user->firstName. ' ' . $user->lastName;
+		}
+		
+		return $recipients;
+	}
+}
