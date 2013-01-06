d5fd59b6ee70914cb1b1a2c07ca9293494b2d05c
Hila Karimov <Hila.Karimov@kaltura.com>
2013-01-06 23:47:52 +0000

    fwr: fixed error in script
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/falcon_2012_08_20@92651 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- deployment/final/sql/02.kaltura_ce_data.sql
+++ deployment/final/sql/02.kaltura_ce_data.sql
@@ -3420,7 +3420,7 @@ insert  into `permission_to_permission_item`(`id`,`permission_id`,`permission_it
 (170554,185,846,now(),now()),
 (170558,200,843,now(),now()),
 (170559,200,761,now(),now()),
-(170559,200,762,now(),now());
+(170560,200,762,now(),now());
 
 insert  into `storage_profile`(`id`,`created_at`,`updated_at`,`partner_id`,`name`,`system_name`,`desciption`,`status`,`protocol`,`storage_url`,`storage_base_dir`,`storage_username`,`storage_password`,`storage_ftp_passive_mode`,`delivery_http_base_url`,`delivery_rmp_base_url`,`delivery_iis_base_url`,`min_file_size`,`max_file_size`,`flavor_params_ids`,`max_concurrent_connections`,`custom_data`,`path_manager_class`,`url_manager_class`,`delivery_priority`,`delivery_status`) values 
 (0,now(),now(),0,'@DC_NAME@','','@DC_DESCRIPTION@',3,0,NULL,'@STORAGE_BASE_DIR@',NULL,NULL,NULL,'@DELIVERY_HTTP_BASE_URL@','@DELIVERY_RTMP_BASE_URL@','@DELIVERY_ISS_BASE_URL@',NULL,NULL,NULL,NULL,NULL,NULL,'',1,1);
