23d8d333b0ce5c2c412f59e9252f23db0f1656ac
Hila Karimov <Hila.Karimov@kaltura.com>
2013-01-27 14:03:56 +0000

    def: 6399 add a parameter in system.ini signifying the location of the response cache dir
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/falcon_2012_08_20@93406 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- alpha/crond/kaltura/clear_cache.sh
+++ alpha/crond/kaltura/clear_cache.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+if [ -L $0 ];then
+	REAL_SCRIPT=`readlink $0`
+else
+	REAL_SCRIPT=$0
+fi
+. `dirname $REAL_SCRIPT`/../../../configurations/system.ini
 
 # Store PID of script:
 # Match script without arguments
@@ -24,11 +30,11 @@ echo $$ > $LCK_FILE
 
 
 echo "`date +%s` start clean v3 `date`" >> $LOG_FILE 2>&1
-#nice -n 19 find /tmp/cache_v3-600 -type f -mmin +1440 -name "cache*" -delete
-/usr/bin/ionice -c3 find /tmp/cache_v3-600 -type f -mmin +1440 -name "cache*" -delete
+#nice -n 19 find $RESPONSE_CACHE_DIR/cache_v3-600 -type f -mmin +1440 -name "cache*" -delete
+/usr/bin/ionice -c3 find $RESPONSE_CACHE_DIR/cache_v3-600 -type f -mmin +1440 -name "cache*" -delete
 echo "`date +%s` end clean v3 `date`" >> $LOG_FILE 2>&1
-#nice -n 19 find /tmp/cache_v2 -type f -mmin +1440 -name "cache*" -delete
-/usr/bin/ionice -c3 find /tmp/cache_v2 -type f -mmin +1440 -name "cache*" -delete
+#nice -n 19 find $RESPONSE_CACHE_DIR/cache_v2 -type f -mmin +1440 -name "cache*" -delete
+/usr/bin/ionice -c3 find $RESPONSE_CACHE_DIR/cache_v2 -type f -mmin +1440 -name "cache*" -delete
 echo "`date +%s` end clean v2 `date`" >> $LOG_FILE 2>&1
 #nice -n 19 find /tmp -maxdepth 0 -type f -mmin +1440 -name "php*" -delete
 /usr/bin/ionice -c3 find /tmp -maxdepth 1 -type f -mmin +1440 -name "php*" -delete
