dd300ce221a904548147cbac28e1215feefb3aa7
Jonathan Kanarek <Jonathan.Kanarek@kaltura.com>
2013-02-28 11:28:43 +0000

    qnd: merges from trunk
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/gemini_2013_02_22@94721 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- scripts/kaltura_batch.sh
+++ scripts/kaltura_batch.sh
@@ -16,18 +16,15 @@ echo `date`
 BATCHDIR=$APP_DIR/batch
 
 BATCHEXE=KGenericBatchMgr.class.php
-# The batch service filename without extensions
-FILE_NAME=${BATCHEXE%.*}
 
 if [ $# != 1 ]; then
 	echo "Usage: $0 [start|stop|restart|status]"
-	#exit 1 chages the return code because this fails the build
 	exit 0 	
 fi
 
 CONFIG_FILE=$APP_DIR/configurations/batch.ini
 
-LOCKFILE="$LOG_DIR/batch.pid"
+LOCKFILE="$LOG_DIR/batch/batch.pid"
 
 echo_success() {
 	[ "$BOOTUP" = "color" ] && $MOVE_TO_COL
@@ -48,7 +45,6 @@ echo_failure() {
 	[ "$BOOTUP" = "color" ] && $SETCOLOR_NORMAL
 	echo -n "]"
 	echo -ne "\r"
-	#return 1
 	return 0
 }
 
@@ -56,20 +52,19 @@ echo_failure() {
 start() {
 	if [ -f $BASE_DIR/maintenance ]; then
 		echo "Server is on maintenance mode - batchMgr will not start!"
-		exit 1
+		return 1
 	fi
 	
 	echo -n $"Starting:"
-	KP=$(pgrep -P 1 -f $FILE_NAME.php)
-	if [ -f $LOCKFILE ]; then
+	KP=$(pgrep -P 1 -f $BATCHEXE)
+	if ! kill -0 `cat $LOCKFILE 2>/dev/null` 2>/dev/null; then 
 		echo_failure
 		echo
 		if [ "X$KP" != "X" ]; then
-			echo "Service $FILE_NAME already running"
-			#return 1
+			echo "Service batch already running"
 			return 0
 		else
-			echo "Service $FILE_NAME isn't running but stale lock file exists"
+			echo "Service batch isn't running but stale lock file exists"
 			echo "Removing stale lock file at $LOCKFILE"
 			rm -f $LOCKFILE
 			start_scheduler
@@ -77,8 +72,8 @@ start() {
 		fi
 	else
 		if [ "X$KP" != "X" ]; then
-			echo "$FILE_NAME is running as $KP without a $LOCKFILE"
-			exit 0
+			echo "Batch is running as $KP without a $LOCKFILE"
+			return 0
 		fi		
 		start_scheduler
 		return 0
@@ -92,7 +87,6 @@ start_scheduler() {
 	if [ "$?" -eq 0 ]; then
 		echo_success
 		echo
-		echo $$ > $LOCKFILE
 	else
 		echo_failure
 		echo
@@ -100,20 +94,19 @@ start_scheduler() {
 }
 
 show_status() {
-		KP=$(pgrep -P 1 -f $FILE_NAME.php) 
+		KP=$(pgrep -P 1 -f $BATCHEXE) 
 		if [ "X$KP" != "X" ]; then
-		echo "$FILE_NAME.php is running as $KP ..."
+		echo "Batch is running as $KP ..."
 		return 0
 		else
-			echo "Service $FILE_NAME isn't running"
-			#return 1
+			echo "Service Batch isn't running"
 			return 0
 		fi
 }
 
 stop() {
 	echo -n $"Shutting down:"
-	KP=$(pgrep -P 1 -f $FILE_NAME.php)
+	KP=$(pgrep -P 1 -f $BATCHEXE)
 	if [ -n "$KP" ]; then
 		PIDS=$(pstree -p $KP | grep -o '[0-9]\{2,5\}')
 		# hack, returnds the PIDS as string and tells kill to kill all at once
@@ -127,7 +120,7 @@ stop() {
 	else
 		echo_failure
 		echo
-		echo "Service $FILE_NAME not running"
+		echo "Service Batch not running"
 		#RC=2
 		RC=0
 	fi
@@ -151,8 +144,7 @@ case "$1" in
 		;;
 	*)
 		echo "Usage: [start|stop|restart|status]"
-		#exit 1
 		exit 0
 		;;
 esac
-exit $?
+exit 0
